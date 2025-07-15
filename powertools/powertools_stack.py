f039f6b9b084686463ccb009b02a93405e78d577
yendoooo <yendoooo1004@gmail.com>
2025-07-16 08:24:27 +0900

    feat: parameters

--- powertools/powertools_stack.py
+++ powertools/powertools_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import Stack
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_logs as logs
+from aws_cdk import aws_ssm as ssm
 from constructs import Construct
 
 
@@ -9,7 +10,7 @@ class PowertoolsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        _lambda.Function(
+        function: _lambda.Function = _lambda.Function(
             self,
             "Function",
             code=_lambda.Code.from_asset_image(
@@ -20,3 +21,11 @@ class PowertoolsStack(Stack):
             runtime=_lambda.Runtime.FROM_IMAGE,
             log_retention=logs.RetentionDays.ONE_DAY,
         )
+
+        param: ssm.StringParameter = ssm.StringParameter(
+            self,
+            "Parameter",
+            string_value='sample value',
+            parameter_name="/lambda-powertools/sample",
+        )
+        param.grant_read(function)
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,7 +1,12 @@
 from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities import parameters
 
 logger: Logger = Logger()
 
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event: dict, context) -> None:
-    logger.info('Hello World!')
+    try:
+        param: str = parameters.get_parameter('/lambda-powertools/sample', max_age=60)
+        logger.info(f'{param=}')
+    except parameters.exceptions.GetParameterError as e:
+        logger.exception(e)
