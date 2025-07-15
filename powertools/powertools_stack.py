f62984764d797f96444cf2a4a8240a949dd31e61
yendoooo <yendoooo1004@gmail.com>
2025-07-16 01:36:35 +0900

    feat: deploy a lambda function

--- powertools/powertools_stack.py
+++ powertools/powertools_stack.py
@@ -1,19 +1,22 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_logs as logs
 from constructs import Construct
+
 
 class PowertoolsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PowertoolsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        _lambda.Function(
+            self,
+            "Function",
+            code=_lambda.Code.from_asset_image(
+                directory='.',
+                file='src/Dockerfile',
+            ),
+            handler=_lambda.Handler.FROM_IMAGE,
+            runtime=_lambda.Runtime.FROM_IMAGE,
+            log_retention=logs.RetentionDays.ONE_DAY,
+        )
