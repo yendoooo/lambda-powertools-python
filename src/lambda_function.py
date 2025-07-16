24cfa95221f2e691c07e0c8fd00391714b441b0a
yendoooo <yendoooo1004@gmail.com>
2025-07-16 13:19:37 +0900

    feat: typing

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,12 +1,14 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities import parameters
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger: Logger = Logger()
 
 @logger.inject_lambda_context(log_event=True)
-def lambda_handler(event: dict, context) -> None:
+def lambda_handler(event: dict, context: LambdaContext) -> None:
+    logger.info(f'{context.get_remaining_time_in_millis()=}')
     try:
-        param: str = parameters.get_parameter('/lambda-powertools/sample', max_age=60)
+        param: str = parameters.get_parameter('/lambda-powertools/sample')
         logger.info(f'{param=}')
     except parameters.exceptions.GetParameterError as e:
         logger.exception(e)
